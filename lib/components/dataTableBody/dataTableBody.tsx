6e39f36bacd043100a23a7f0eacf75b5f6c9985c
kguie <kevin@sofy.fr>
2025-08-06 00:32:04 -0400

    ajust data table body style

--- lib/components/dataTableBody/dataTableBody.tsx
+++ lib/components/dataTableBody/dataTableBody.tsx
@@ -4,57 +4,74 @@ import { useDataTableContext } from "../../contexts/dataTableContext";
 import { getNestedValue } from "../../utils/utils";
 
 export function DataTableBody() {
-    const { paginatedData, columns, setSortKey, sortKey, setSortAsc, sortAsc, pageSize, setPageSize, filteredData, hasPagination } = useDataTableContext<any>();
+    const {
+        paginatedData, columns,
+        setSortKey, sortKey,
+        setSortAsc, sortAsc,
+        pageSize, setPageSize,
+        filteredData, hasPagination
+    } = useDataTableContext<any>();
     const dataToDisplay = hasPagination ? paginatedData : filteredData;
 
-    return <>
-        <PaginationHeader on={hasPagination} total={filteredData.length} pageSize={pageSize} setPageSize={setPageSize} />
-        <table className="border-collapse mt-2">
-            <thead className="bg-zinc-600">
-                <tr className="overflow-hidden">
-                    {columns.map((col, index) => (
-                        <th
-                            key={col.key}
-                            className={
-                                `text-left text-white cursor-pointer relative py-2 pr-6 group ${index === 0 ? 'pl-4 rounded-l-lg' : 'pl-2'} 
-                                ${index === columns.length - 1 ? 'rounded-r-lg' : ''}`}
-                            onClick={() =>
-                                col.sortable &&
-                                (setSortKey(col.key),
-                                    setSortAsc(sortKey === col.key ? !sortAsc : true))
-                            }>
-                            <span  >{col.title}</span>
-                            <TableHeaderIcon visible={sortKey === col.key} sortAsc={sortAsc} />
-                        </th>
-                    ))}
-                </tr>
-            </thead>
-            <tbody>
-                {dataToDisplay.map((row, rowIndex) => (
-                    <tr key={rowIndex}>
-                        {columns.map((col, colIndex) => (
-                            <td className={`${rowIndex === 0 ? "pt-4 pb-1" : "py-1"} ${colIndex === 0 ? "pl-4" : "pl-2"} pr-2`}
-                                key={col.key}>{String(getNestedValue(row, col.key))}</td>
+    return (
+        <>
+            <PaginationHeader
+                on={hasPagination}
+                total={filteredData.length}
+                pageSize={pageSize}
+                setPageSize={setPageSize}
+            />
+            <table className="table-fixed w-full border-collapse mt-2">
+                <thead className="bg-zinc-600">
+                    <tr>
+                        {columns.map((col, i) => (
+                            <th
+                                key={col.key}
+                                title={col.title}
+                                className={`
+                 cursor-pointer py-2 px-4
+                  ${i === 0 ? "rounded-l-lg" : ""}
+                  ${i === columns.length - 1 ? "rounded-r-lg" : ""}
+                `}
+                                onClick={() => {
+                                    if (!col.sortable) return;
+                                    setSortKey(col.key);
+                                    setSortAsc(sortKey === col.key ? !sortAsc : true);
+                                }}
+                            ><div className="flex items-center justify-between
+                  text-left text-white">
+                                    <span className="flex-1 truncate">{col.title}</span>
+                                    {<ChevronUpIcon
+                                        aria-hidden="true"
+                                        className={`
+                    flex-shrink-0 w-4 h-4 ml-1
+                    transform transition-transform duration-200
+                    ${col.sortable && sortKey === col.key
+                                                ? (col.sortable && sortAsc ? "rotate-0" : "rotate-180")
+                                                : "opacity-0 group-hover:opacity-100"}
+                  `}
+                                    />}
+                                </div>
+                            </th>
                         ))}
                     </tr>
-                ))}
-            </tbody>
-        </table>
-    </>
-}
-
-function TableHeaderIcon({ visible, sortAsc }: { visible: boolean; sortAsc: boolean }) {
-    return (
-
-        <div className="absolute inset-0 flex items-center justify-end pt-1 pr-2">
-            <ChevronUpIcon
-                aria-hidden="true"
-
-                className={`w-4 h-4 transform transition-transform duration-200 ${visible ? (sortAsc ? 'rotate-0' : 'rotate-180') : 'opacity-0 group-hover:opacity-100'
-                    }`}
-            />
-        </div>
-
+                </thead>
+                <tbody>
+                    {dataToDisplay.map((row, rowIndex) => (
+                        <tr key={rowIndex} >
+                            {columns.map((col, colIndex) => (
+                                <td
+                                    key={col.key}
+                                    className={`py-2 px-4 ${colIndex === 0 ? "pl-6" : ""}`}
+                                >
+                                    {String(getNestedValue(row, col.key))}
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
     );
 }
 
